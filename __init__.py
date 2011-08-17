ca1df82466d7ab13595a699c61308f1c690f3b41
Javier Cordero <jneight@gmail.com>
2011-08-17 11:19:08 +0200

    license added

--- __init__.py
+++ __init__.py
@@ -1,23 +1,26 @@
 # coding=utf-8
 
-__author__ = "Javier Cordero Martínez (javier@georemindme.com)"
+__author__ = "Javier Cordero Martinez (javier@georemindme.com)"
 __copyright__ = "Copyright 2011"
 __contributors__ = []
 __license__ = "AGPLv3"
 __version__ = "0.1"
 """
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+    Vavag python client>
+    Copyright (C) 2011  Javier Cordero Martinez
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
 
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 
